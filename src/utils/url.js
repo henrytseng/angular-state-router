63716c13be7e14e5bb8a2d323b89c16426ae623e
Henry Tseng <henryiis@gmail.com>
2015-07-14 00:23:26 -0400

    Updated Url to use functional object creation

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,29 +1,31 @@
 'use strict';
 
-function Url() {
+function Url(url) {
+  url = url || '';
 
+  return {
+
+    /**
+     * Get the path of a URL
+     * 
+     * @return {String}     A querystring from URL
+     */
+    path: function() {
+      var inflected = url.replace(/\?.*/, '');
+      return inflected;
+    },
+
+    /**
+     * Get the querystring of a URL
+     * 
+     * @return {String}     A querystring from URL
+     */
+    querystring: function() {
+      var inflected = url.replace(/.*\?/, '');
+      return inflected;
+    }
+  };
 }
 
-/**
- * Get the path of a URL
- * 
- * @param  {String} url A URL to test for
- * @return {String}     A querystring from URL
- */
-Url.path = function(url) {
-  var inflected = (url || '').replace(/\?.*/, '');
-  return inflected;
-};
-
-/**
- * Get the querystring of a URL
- * 
- * @param  {String} url A URL to test for
- * @return {String}     A querystring from URL
- */
-Url.querystring = function(url) {
-  var inflected = (url || '').replace(/.*\?/, '');
-  return inflected;
-};
 
 module.exports = Url;
--- tests/spec/utils/url.spec.js
+++ tests/spec/utils/url.spec.js
@@ -5,13 +5,13 @@ describe('Url', function() {
 
   describe('#path', function() {
     it('Should get url path', function() {
-      expect(Url.path('/pets/cat/color?hue=green')).toBe('/pets/cat/color');
+      expect(Url('/pets/cat/color?hue=green').path()).toBe('/pets/cat/color');
     });
   });
 
   describe('#querystring', function() {
     it('Should get url querystring', function() {
-      expect(Url.querystring('/pets/cat/color?hue=green&tint=2390')).toBe('hue=green&tint=2390');
+      expect(Url('/pets/cat/color?hue=green&tint=2390').querystring()).toBe('hue=green&tint=2390');
     });
   });
 
